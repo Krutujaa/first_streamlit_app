7d8281f932d4014f8d17131fc48ab48cfbc630af
Krutujaa <86482355+Krutujaa@users.noreply.github.com>
2023-11-08 13:51:45 +0530

    Update streamlit_app.py
    Typo Error
    Added Header
    Added texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.tile('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale,Spinach & Rocket Smoothie')
+streamlit.text('Hard- Boiled Free-Range Egg')
